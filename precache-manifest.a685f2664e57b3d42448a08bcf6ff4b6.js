d4b792100da4bcdf494c17eb4f9539b1dc2303dc
RaquelCC <rcanalesconcha@gmail.com>
2019-06-18 18:10:40 -0400

    Updates

---
+++ precache-manifest.a685f2664e57b3d42448a08bcf6ff4b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6e2ad262e576325df69109af8756889",
+    "url": "/desafio-penta/index.html"
+  },
+  {
+    "revision": "4ec37e493a012665e4b6",
+    "url": "/desafio-penta/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fd4732fca148c4ab768c",
+    "url": "/desafio-penta/static/js/2.448e30e7.chunk.js"
+  },
+  {
+    "revision": "4ec37e493a012665e4b6",
+    "url": "/desafio-penta/static/js/main.613b9ddf.chunk.js"
+  },
+  {
+    "revision": "f16aa3ab8f9be4162dd5",
+    "url": "/desafio-penta/static/js/runtime~main.b3772cf0.js"
+  }
+]);
